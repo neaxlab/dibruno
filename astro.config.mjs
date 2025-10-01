5ffad8898aea039ad79e70019f67c842152206f4
leobahm <leobahm72@gmail.com>
2025-10-01 13:03:43 -0500

    fix: change adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 
 
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: node({ mode: 'standalone' }), // puedes usar edge, vercel, etc.
+    adapter: vercel(),
     integrations: [tailwind(), react()],
 });
