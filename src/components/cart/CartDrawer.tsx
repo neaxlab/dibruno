2f3f8bb77a7cafa3e2cb7aa5c2072eaa3ace5f58
Jusedu <sebas2812.jd@gmail.com>
2025-10-07 14:53:05 -0500

    feat: enhance CartDrawer component with improved item display, updated styles, and new empty cart message; add terms acceptance functionality

--- src/components/cart/CartDrawer.tsx
+++ src/components/cart/CartDrawer.tsx
@@ -17,6 +17,8 @@ const CartDrawer: React.FC = () => {
   const [cartData, setCartData] = useState(cart.get());
   const [termsAccepted, setTermsAccepted] = useState(false);
 
+  const hasItems = !!(cartData && cartData.lines?.nodes.length);
+
   useEffect(() => {
     const unsubscribe = cart.subscribe(setCartData);
     return unsubscribe;
@@ -38,11 +40,9 @@ const CartDrawer: React.FC = () => {
     const unsubscribe = isCartDrawerOpen.subscribe((open) => {
       if (open) {
         setIsOpen(true);
-        // Pequeño delay para permitir que el DOM se actualice antes de la animación
         setTimeout(() => setIsAnimating(true), 10);
       } else {
         setIsAnimating(false);
-        // Esperar a que termine la animación de salida antes de ocultar
         setTimeout(() => setIsOpen(false), 500);
       }
     });
@@ -119,17 +119,17 @@ const CartDrawer: React.FC = () => {
             ref={clickOutsideRef}
             onKeyDown={onKeyDown}
           >
-            <div className={`pointer-events-auto w-screen sm:max-w-[50vw] max-h-screen bg-white transition-transform duration-500 ease-in-out transform ${isAnimating
+            <div className={`pointer-events-auto w-screen max-w-[35dvw] max-h-screen bg-white transition-transform duration-500 ease-in-out transform ${isAnimating
               ? 'translate-x-0'
               : 'translate-x-full'
               }`}>
               <div className="flex flex-col min-h-full max-h-screen bg-primary-bright">
-                <div className="flex items-center justify-between p-5">
+                <div className="flex items-center justify-between p-5 border-b-[0.5px] border-[#BABABA]">
                   <h2
-                    className="text-2xl flex gap-4 items-center text-black"
+                    className="text-[24px] font-normal flex gap-4 items-center text-black"
                     id="slide-over-title"
                   >
-                    <span className="text-d-title-1">Cart ({cartData?.totalQuantity})</span>
+                    <span className="text-[24px] font-normal">Cart ({cartData?.totalQuantity})</span>
                     {isUpdating && (
                       <svg
                         className="animate-spin -ml-1 mr-3 h-4 w-4"
@@ -152,7 +152,7 @@ const CartDrawer: React.FC = () => {
                     >
                       <span className="sr-only">Cerrar panel</span>
                       <svg
-                        className="h-11 w-11 hover:rotate-90 transition-all duration-300"
+                        className="h-8 w-8 hover:rotate-90 transition-all duration-300"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -172,13 +172,13 @@ const CartDrawer: React.FC = () => {
 
 
                 <div className="flex-1 overflow-y-auto hide-scrollbar" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-                  <div className="px-6 min-h-full flex flex-col">
+                  <div className="min-h-full flex flex-col">
                     {cartData && cartData.lines?.nodes.length > 0 ? (
                       <div
                         className={`gap-10 ${cartIsUpdatingClass}`}
                       >
                         {cartData.lines.nodes.map((item) => (
-                          <div key={item.id} className="flex py-8 gap-6 flex-row justify-between items-start ">
+                          <div key={item.id} className="flex py-8 px-6  gap-6 flex-row justify-between items-start border-b-[0.5px] border-[#BABABA]">
                             <div className="flex items-center gap-6 flex-row">
                               <div className="overflow-hidden w-[136px] h-[136px] flex-shrink-0 rounded-3xl">
                                 <img
@@ -189,21 +189,21 @@ const CartDrawer: React.FC = () => {
                               </div>
                               <div className="flex-1 flex flex-col justify-between items-start gap-6">
                                 <div className="flex flex-col gap-2">
-                                  <h3 className="sm:text-d-products text-d-secondary">
+                                  <h3 className="!text-[18px] sm:!text-[18px] font-medium text-d-secondary">
                                     {item.merchandise.product.title}
                                   </h3>
-                                  <Money price={item.cost.amountPerQuantity} className="text-primary-olive sm:text-d-products text-d-secondary" />
+                                  <Money price={item.cost.amountPerQuantity} className="text-primary-olive !text-[18px] sm:!text-[18px] font-light text-d-secondary" />
                                 </div>
-                                <div className="flex items-center gap-4">
-                                  <div className="flex items-center gap-6 p-2 rounded-full border border-primary-granite">
+                                <div className="flex items-center justify-between w-full">
+                                  <div className="flex items-center gap-6 p-2 rounded-full border-[0.5px] border-[#67645E]">
                                     <button
                                       onClick={() => decreaseQuantity(item.id, item.quantity)}
                                       type="button"
                                       disabled={isUpdating}
-                                      className="w-[26px] h-[26px] flex items-center justify-center disabled:opacity-50 cursor-pointer bg-primary-granite rounded-full"
+                                      className=" flex items-center justify-center disabled:opacity-50 cursor-pointer rounded-full text-primary-granite"
                                     >
-                                      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 14 3" fill="none">
-                                        <path d="M12.3332 1.5H1.6665" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 14 3" fill="none">
+                                        <path d="M12.3332 1.5H1.6665" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                       </svg>
                                     </button>
                                     <span className="text-d-secondary text-primary-granite">
@@ -213,110 +213,98 @@ const CartDrawer: React.FC = () => {
                                       onClick={() => increaseQuantity(item.id, item.quantity)}
                                       type="button"
                                       disabled={isUpdating || item.quantity >= 99}
-                                      className={`w-[26px] h-[26px] flex items-center justify-center disabled:opacity-50 cursor-pointer rounded-full ${item.quantity >= 99 ? 'bg-[#D0CFCE]' : 'bg-primary-granite'}`}
+                                      className={` flex items-center justify-center disabled:opacity-50 cursor-pointer rounded-full ${item.quantity >= 99 ? 'text-[#D0CFCE]' : ' text-primary-granite'}`}
                                     >
-                                      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 14 13" fill="none">
-                                        <path d="M6.99984 1.1665V11.8332M12.3332 6.49984H1.6665" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 14 13" fill="none">
+                                        <path d="M6.99984 1.1665V11.8332M12.3332 6.49984H1.6665" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                       </svg>
                                     </button>
                                   </div>
+                                  <button
+                                    onClick={() => removeItem(item.id)}
+                                    type="button"
+                                    disabled={isUpdating}
+                                    className="text-[#717171] text-[16px] font-light leading-[100%] tracking-[0.32px]"
+                                  >
+                                    Remove
+                                  </button>
                                 </div>
                               </div>
                             </div>
-                            <button
-                              onClick={() => removeItem(item.id)}
-                              type="button"
-                              disabled={isUpdating}
-                              className=" disabled:opacity-50 w-fit text-sm font-medium underline cursor-pointer"
-                            >
-                              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                                <path d="M26 7.3335L25.1737 20.7003C24.9625 24.1154 24.8571 25.823 24.0011 27.0507C23.5777 27.6576 23.0329 28.1699 22.4009 28.5548C21.1228 29.3335 19.412 29.3335 15.9903 29.3335C12.5642 29.3335 10.8511 29.3335 9.57207 28.5534C8.93973 28.1678 8.39467 27.6546 7.97157 27.0466C7.11584 25.817 7.0126 24.107 6.80615 20.6871L6 7.3335" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" />
-                                <path d="M4 7.33317H28M21.4076 7.33317L20.4975 5.45548C19.8928 4.20818 19.5904 3.58453 19.0689 3.19558C18.9533 3.1093 18.8308 3.03256 18.7027 2.9661C18.1252 2.6665 17.4321 2.6665 16.046 2.6665C14.6251 2.6665 13.9147 2.6665 13.3276 2.97866C13.1975 3.04785 13.0733 3.1277 12.9564 3.2174C12.4289 3.6221 12.1342 4.26857 11.5448 5.56152L10.7372 7.33317" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" />
-                                <path d="M12.667 22V14" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" />
-                                <path d="M19.333 22V14" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" />
-                              </svg>
-                            </button>
+
                           </div>
                         ))}
                       </div>
                     ) : (
                       <div className="flex flex-col gap-4 justify-center items-center flex-1 min-h-[400px]">
-                        <p className="sm:text-d-title-2 text-d-secondary text-2xl text-primary-granite">Your Cart is Empty</p>
-                        <ButtonSlide
-                          text={`BROWSER PRODUCTS`}
-                          href="/shop"
-                          normalBackground="transparent"
-                          normalColor="#3B3B3B"
-                          hoverBackground="#3B3B3B"
-                          hoverColor="#FAFAFA"
-                          borderColor="#3B3B3B"
-                          hoverBorderColor="#FAFAFA"
-                          transitionDuration="0.5s"
-                          className="w-full justify-center items-center flex"
-                          disabled={!termsAccepted}
-                        />
+                        <p className="text-[#949494] text-[24px] font-semibold leading-normal tracking-[0.48px] capitalize">Your Cart is Empty</p>
+                        <button
+                          type="button"
+                          onClick={() => { window.location.href = '/shop'; }}
+                          className="w-full justify-center items-center flex text-[#3B3B3B] text-[16px] font-semibold leading-normal tracking-[0.32px] underline decoration-solid capitalize"
+                        >
+                          Browse products
+                        </button>
                       </div>
                     )}
                   </div>
                 </div>
 
                 <div>
-                  {cartData && cartData.lines?.nodes.length > 0 && (
-                    <div className="p-6 flex flex-col gap-3">
-                      <div className="flex justify-between items-center">
-                        <p className="sm:text-d-title-2 text-d-secondary text-2xl">Subtotal</p>
-                        <p>
-                          <Money
-                            price={cartData.cost.subtotalAmount}
-                            showCurrency={true}
-                            className="sm:text-d-title-2 text-d-secondary text-2xl"
-                          />
-                        </p>
+                  <div className="p-6 flex flex-col gap-3">
+                    <div className="flex justify-between items-center">
+                      <p className="text-[#3B3B3B] text-[24px] font-medium leading-[120%] tracking-[0.48px]">Subtotal</p>
+                      <p>
+                        <Money
+                          price={cartData && cartData.lines?.nodes.length ? cartData.cost.subtotalAmount : { amount: '0.00', currencyCode: (cartData?.cost?.subtotalAmount?.currencyCode || 'USD') as string }}
+                          showCurrency={true}
+                          className="text-[#3B3B3B] text-[24px] font-medium leading-[120%] tracking-[0.48px]"
+                        />
+                      </p>
 
-                      </div>
-                      <div className="w-full flex items-center gap-4">
-                        <div className="relative">
-                          <input
-                            type="checkbox"
-                            id="terms-checkbox"
-                            className="relative size-6 cursor-pointer appearance-none rounded-[4px] bg-white border-2 border-primary-olive checked:bg-primary-olive checked:border-primary-olive focus:outline-none focus:ring-2 focus:ring-primary-olive/20"
-                            checked={termsAccepted}
-                            onChange={handleTermsChange}
-                          />
-                          {termsAccepted && (
-                            <svg
-                              className="absolute top-3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5 h-5 text-primary-lotion pointer-events-none"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          )}
-                        </div>
-                        <label htmlFor="terms-checkbox" className="sm:text-d-products text-d-tertiary font-light">I agree to the <a href="/terms-of-service" className="text-primary-olive underline">Terms and Policies</a></label>
-                      </div>
-                      <ButtonSlide
-                        text={`SHOP NOW`}
-                        href={cartData.checkoutUrl}
-                        normalBackground="transparent"
-                        normalColor="#3B3B3B"
-                        hoverBackground="#3B3B3B"
-                        hoverColor="#FAFAFA"
-                        borderColor="#3B3B3B"
-                        hoverBorderColor="#FAFAFA"
-                        transitionDuration="0.5s"
-                        className="w-full justify-center items-center flex sm:mt-6 mt-3"
-                        disabled={!termsAccepted}
-                      />
-                      <div className="text-xs sm:text-d-tertiary font-light w-full text-primary-granite">
-                        *Shipping, taxes and discounts calculated at checkout
-                      </div>
                     </div>
-                  )}
+                    <div className="text-xs sm:text-d-tertiary font-light w-full text-primary-granite">
+                      Shipping, taxes and discounts calculated at checkout
+                    </div>
+                    <div className="w-full flex items-center gap-4">
+                      <div className="relative">
+                        <input
+                          type="checkbox"
+                          id="terms-checkbox"
+                          className="relative size-6 cursor-pointer appearance-none rounded-[4px] bg-white border-2 border-primary-olive checked:bg-primary-olive checked:border-primary-olive focus:outline-none focus:ring-2 focus:ring-primary-olive/20 disabled:opacity-50 disabled:cursor-not-allowed"
+                          checked={termsAccepted}
+                          disabled={!hasItems}
+                          onChange={handleTermsChange}
+                        />
+                        {termsAccepted && (
+                          <svg
+                            className="absolute top-3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[22px] h-[22px] pointer-events-none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="22"
+                            height="22"
+                            viewBox="0 0 22 22"
+                            fill="none"
+                          >
+                            <path d="M4.58203 12.833L7.79036 16.0413L17.4154 5.95801" stroke="#FEFEFE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                          </svg>
+                        )}
+                      </div>
+                      <label htmlFor="terms-checkbox" className="text-[#3B3B3B] text-[16px] font-light leading-[120%] tracking-[0.32px]">I agree to the <a href="/terms-of-service" className="text-primary-olive underline">Terms and Policies</a></label>
+                    </div>
+                    <ButtonSlide
+                      text={`SHOP NOW`}
+                      href={cartData?.checkoutUrl || '/shop'}
+                      normalBackground="transparent"
+                      normalColor="#3B3B3B"
+                      hoverBackground="#3B3B3B"
+                      hoverColor="#FAFAFA"
+                      borderColor="#3B3B3B"
+                      hoverBorderColor="#FAFAFA"
+                      transitionDuration="0.5s"
+                      className="w-full justify-center items-center flex sm:mt-6 mt-3"
+                      disabled={!termsAccepted || !hasItems}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
